53eced5bb75556fea066d6a4fb83fd56829d1447
Seth Krasnianski <seth@figmints.com>
2014-07-27 18:22:21 -0400

    form view

---
+++ form.php
@@ -0,0 +1,7 @@
+<div id="figchimp">
+  <h3><?php echo $title; ?></h3>
+  <form>
+    <input class="email text input" name="" type="text" placeholder="Enter your email">
+    <input class="submit small button submit" type="submit" value="Submit">
+  </form>
+</div>
